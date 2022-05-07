2289ef43c16c12c0fa5b70c97a17bc1504b74aac
iulia sirbu <iuliasirbu47@gmail.com>
2022-05-07 17:30:31 +0300

    Initial setup

--- src/javascript/components/MainPage.jsx
+++ src/javascript/components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MainPage(props) {
     return (
         <div id="MainPage">
-
+            <h1 className="mt-10 text-purple-600 text-bold text-3xl">Main Page</h1>
         </div>
     )
 }
